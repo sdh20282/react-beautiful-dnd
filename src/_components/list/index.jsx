fee6a1bfff567fee961f7ef4c3fb7b400c83832a
sdh20282 <sdh20282@gmail.com>
2024-08-28 17:33:13 +0900

    refactor: list 컴포넌트에 styled-component 추가

--- src/_components/list/index.jsx
+++ src/_components/list/index.jsx
@@ -4,18 +4,20 @@ import { Droppable } from "react-beautiful-dnd";
 
 import { getListStyle } from '@utils';
 
+import * as s from './styles';
+
 const List = ({ children, id }) => {
   return (
     <Droppable droppableId={id}>
       {(provided, snapshot) => (
-        <div
+        <s.ListStyle
           {...provided.droppableProps}
           ref={provided.innerRef}
           style={getListStyle(snapshot.isDraggingOver)}
         >
           {children}
           {provided.placeholder}
-        </div>
+        </s.ListStyle>
       )}
     </Droppable>
   )
